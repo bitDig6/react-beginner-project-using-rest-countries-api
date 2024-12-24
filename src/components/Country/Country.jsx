4f102e1f56b82b8e1f41113f2e0a7ceaf39faf5a
bitDig6 <ummenabilajalal@gmail.com>
2024-12-24 09:10:52 +0600

    added visited status button and used conditional rendering for styling and change the button text

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,30 @@
+import { useState } from 'react';
 import './Country.css'
 const Country = ({country}) => {
     console.log(country);
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
+
+    const colorVisited = {
+        backgroundColor: visited ? 'bisque' : 'white'
+    }
+
     return (
-        <div className="country-container">
+        <div style={colorVisited} className="country-container">
             <img src={country.flags.png} alt="" />
             <h4>Country Name: {country.name.common}</h4>
             <p>Capital: {country.capital}</p>
             <p>Area: {country.area} square kilometers</p>
             <p>Population: {country.population}</p>
+            <div className='button-container'>
+                <button onClick={handleVisited}>
+                    {visited ? 'Visited' : 'Going'}
+                </button>
+            </div>
         </div>
     );
 };
